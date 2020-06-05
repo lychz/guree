23e7aa528b9df1bf4f1b4b5af09651fc5494509d
lychz <lyc_hz@outlook.com>
2020-06-05 20:28:45 +0800

    finish Icon component

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { SVGAttributes } from "react";
 import ReactDOM from "react-dom";
 import "./importIcons.tsx";
 import "./Icon.scss";
+import { type } from "os";
 
-interface Props {
+interface Props{
+  /** 图标名称 */
   name: string;
+  /** 图标大小，可以是数字或字符串，比如 10 1em 20px 等，数字默认单位是 px */
+  size?: string | number;
+  /** 图标颜色 */
+  fill?: string;
+  /** 点击图标触发 */
+  onClick?: React.MouseEventHandler<SVGElement>
 }
 
-const Icon: React.FunctionComponent<Props> = props => {
+const Icon: React.FunctionComponent<Props> = ({ name, size = "1em", fill, onClick }: Props) => {
+
+  const sizeValue = (size?: string | number) => {
+    return typeof size === 'number' ? `${size}px` : size
+  }
+
   return (
-    <svg className="icon">
-      <use xlinkHref={`#${props.name}`} />
+    <svg 
+      className="icon" 
+      style={{
+        width: sizeValue(size),
+        height: sizeValue(size),
+        fill
+      }}
+      onClick={onClick}
+    >
+      <use xlinkHref={`#${name}`}/>
     </svg>
   );
 };
 
-export default Icon;
+export default Icon
--- src/components/Icon/Readme.md
+++ src/components/Icon/Readme.md
@@ -1,6 +1,24 @@
-```tsx
-<Icon>123</Icon>
-```
+图标集见 fontawesome 的 `free && brands &&  regular && solid` 分类，参见[此地址](https://fontawesome.com/icons?d=gallery&s=brands,regular,solid&m=free)
+### 基本用法
+使用 `name` 设置图标名称
 ```jsx
 <Icon name="adn"></Icon>
 ```
+### 图标大小
+使用 `size` 设置图标大小
+```jsx
+const value = 40;
+<div>
+  <Icon name="adn" size="0.5em"></Icon>
+  <Icon name="adn" size="18px"></Icon>  
+  <Icon name="adn" size={value}></Icon>
+</div>
+```
+### 图标颜色
+使用 `fill` 设置图标颜色
+```jsx
+  <div>
+    <Icon name="adn" fill="red"></Icon>
+  </div>
+```
+
