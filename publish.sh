c4f83bda4f01da3ef781164a9f3a77d122918616
lychz <lyc_hz@outlook.com>
2020-06-05 22:00:54 +0800

    update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 yarn build && \
 yarn styleguide:build && \
-git commit -m"update document"&& \
+git add -A && \
+git commit -m update document && \
 npm version ${1:-patch} && \
 git push
