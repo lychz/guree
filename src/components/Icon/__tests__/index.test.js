d55f70ed4a75a42645adc6c6aaed82dc9cb74ae0
lychz <lyc_hz@outlook.com>
2020-06-05 21:20:41 +0800

    update Icon components unit tests

--- src/components/Icon/__tests__/index.test.js
+++ src/components/Icon/__tests__/index.test.js
@@ -1,11 +1,30 @@
-import React from 'react'
-import renderer from 'react-test-renderer'
-import Icon from '..'
-// import "../importIcons";
+import React from "react";
+import renderer from "react-test-renderer";
+import Icon from "..";
+import { mount } from "enzyme";
 
-it('Icon exports modules correctly', () => {
-  const icon = renderer
-    .create(<Icon name="adn"></Icon>)
-    .toJSON();
-  expect(icon).toMatchSnapshot();
+describe("Icon 组件", () => {
+  it("正确导出 Icon 组件", () => {
+    const icon = renderer.create(<Icon name="adn"></Icon>).toJSON();
+    expect(icon).toMatchSnapshot();
+  });
+
+  it("测试 Icon 组件 size 参数", () => {
+    const numberType = renderer.create(<Icon size={10}></Icon>).toJSON();
+    const stringType = renderer.create(<Icon size="1em"></Icon>);
+    expect(numberType).toMatchSnapshot();
+    expect(stringType).toMatchSnapshot();
+  });
+
+  it("测试 Icon 组件 fill 参数", () => {
+    const icon = renderer.create(<Icon fill="blue"></Icon>).toJSON();
+    expect(icon).toMatchSnapshot();
+  });
+
+  it("测试 Icon 组件 onClick 事件", () => {
+    const fn = jest.fn();
+    const icon = mount(<Icon name="adn" onClick={fn}></Icon>);
+    icon.find("svg").simulate("click");
+    expect(fn).toBeCalled();
+  });
 });
--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -1,5 +1,5 @@
-describe('build files', () => {
-  it('build exports modules correctly', () => {
+describe('打包组件', () => {
+  it('正确导出组件包', () => {
     const guree = require('..');
     expect(Object.keys(guree)).toMatchSnapshot();
   });
