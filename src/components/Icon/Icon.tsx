ad5dec24ff6dfffa0e81d51bf4076596bcd9d6a3
lychz <lyc_hz@outlook.com>
2020-06-07 20:00:34 +0800

    add scoped className for Icon

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,10 +1,9 @@
-import React, { SVGAttributes } from "react";
-import ReactDOM from "react-dom";
+import React from "react";
 import "./importIcons.tsx";
 import "./Icon.scss";
-import { type } from "os";
+import { scopedClass } from "@utils/index";
 
-interface Props{
+interface Props {
   /** 图标名称 */
   name: string;
   /** 图标大小，可以是数字或字符串，比如 10 1em 20px 等，数字默认单位是 px */
@@ -12,28 +11,32 @@ interface Props{
   /** 图标颜色 */
   fill?: string;
   /** 点击图标触发 */
-  onClick?: React.MouseEventHandler<SVGElement>
+  onClick?: React.MouseEventHandler<SVGElement>;
 }
 
-const Icon: React.FunctionComponent<Props> = ({ name, size = "1em", fill, onClick }: Props) => {
-
+const Icon: React.FunctionComponent<Props> = ({
+  name,
+  size = "1em",
+  fill,
+  onClick,
+}: Props) => {
   const sizeValue = (size?: string | number) => {
-    return typeof size === 'number' ? `${size}px` : size
-  }
+    return typeof size === "number" ? `${size}px` : size;
+  };
 
   return (
-    <svg 
-      className="icon" 
+    <svg
+      className={scopedClass("icon")}
       style={{
         width: sizeValue(size),
         height: sizeValue(size),
-        fill
+        fill,
       }}
       onClick={onClick}
     >
-      <use xlinkHref={`#${name}`}/>
+      <use xlinkHref={`#${name}`} />
     </svg>
   );
 };
 
-export default Icon
+export default Icon;
