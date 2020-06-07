1c0646112543c5d8d1915453e6cbb8b89d86a13f
lychz <lyc_hz@outlook.com>
2020-06-07 19:20:36 +0800

    update alias

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -255,8 +255,8 @@ module.exports = function(webpackEnv) {
         // Support React Native Web
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
-        '@': path.resolve(__dirname, '../src/components'),
-        'utils': path.resolve(__dirname, '../src/common/utils'),
+        '@components': path.resolve(__dirname, '../src/components'),
+        '@utils': path.resolve(__dirname, '../src/common/utils'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
