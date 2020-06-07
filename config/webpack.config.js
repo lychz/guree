deb2b3c020d6770260601e7f3d55b4312f3a0333
lychz <lyc_hz@outlook.com>
2020-06-07 17:20:15 +0800

    set utils alias to utils

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -256,6 +256,7 @@ module.exports = function(webpackEnv) {
         // https://www.smashingmagazine.com/2016/08/a-glimpse-into-the-future-with-react-native-for-web/
         'react-native': 'react-native-web',
         '@': path.resolve(__dirname, '../src/components'),
+        'utils': path.resolve(__dirname, '../src/common/utils'),
       },
       plugins: [
         // Adds support for installing with Plug'n'Play, leading to faster installs and adding
