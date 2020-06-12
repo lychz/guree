f2cf6a6b993483d8819cea096ce9cd02dd32fb17
lychz <lyc_hz@outlook.com>
2020-06-12 19:03:50 +0800

    update styleguide

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,37 +1,59 @@
-const path = require('path');
-const { version } = require('./package');
-const webpackConfig = require('./config/webpack.config.js')()
+const path = require("path");
+const { version } = require("./package");
+const webpackConfig = require("./config/webpack.config.js")();
 
 module.exports = {
-	components: 'src/components/**/[A-Z]*.tsx',
-	defaultExample: true,
-	moduleAliases: {
-		'@components': path.resolve(__dirname, 'src/components'),
-	},
-	ribbon: {
-		url: 'https://github.com/styleguidist/react-styleguidist',
-	},
-	webpackConfig: {
-		resolve: webpackConfig.resolve,
-		module: webpackConfig.module,
-	},
-	version,
-	propsParser: require('react-docgen-typescript').withCustomConfig(
-		'./tsconfig.json',
+  components: "src/components/**/[A-Z]*.tsx",
+  defaultExample: true,
+  moduleAliases: {
+    "@components": path.resolve(__dirname, "src/components"),
+  },
+  ribbon: {
+    url: "https://github.com/styleguidist/react-styleguidist",
+  },
+  webpackConfig: {
+    resolve: webpackConfig.resolve,
+    module: webpackConfig.module,
+  },
+  version,
+  propsParser: require("react-docgen-typescript").withCustomConfig(
+    "./tsconfig.json",
+    {
+      shouldExtractLiteralValuesFromEnum: true,
+      savePropValueAsString: true,
+      shouldRemoveUndefinedFromOptional: true,
+      propFilter: (prop, component) => {
+        if (prop.parent) {
+          return !prop.parent.fileName.includes("node_modules");
+        }
+
+        return true;
+      },
+    }
+  ).parse,
+  require: [path.join(__dirname, "styleguide.styles.css")],
+  sortProps: (props) => props,
+  pagePerSection: true,
+	sections: [
+    {
+      name: '介绍',
+      content: 'introduction.md'
+    },
+    {
+      name: '基础组件',
+      components: [
+				'src/components/Button/Button.tsx',
+				'src/components/Icon/Icon.tsx',
+			],
+			sectionDepth: 1
+		},
 		{
-			shouldExtractLiteralValuesFromEnum: true,
-			savePropValueAsString: true,
-			shouldRemoveUndefinedFromOptional: true,
-			propFilter: (prop, component) => {
-				if (prop.parent) {
-					return !prop.parent.fileName.includes('node_modules')
-				}
-				
-				return true
-			}
-		}
-	).parse,
-	require: [
-    path.join(__dirname, 'styleguide.styles.css')
+      name: '布局组件',
+      components: [
+				'src/components/Col/Col.tsx',
+				'src/components/Row/Row.tsx',
+			],
+			sectionDepth: 1
+    },
   ]
 };
