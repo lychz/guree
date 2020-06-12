c37d3606decdf042c3d4fad1cc3b64682791f75a
lychz <lyc_hz@outlook.com>
2020-06-12 21:32:07 +0800

    export Row and Col

--- src/index.tsx
+++ src/index.tsx
@@ -1,2 +1,4 @@
 export { default as Button } from "./components/Button";
 export { default as Icon } from "./components/Icon";
+export { default as Row } from "./components/Row";
+export { default as Col } from "./components/Col";
