f5a05f3d5433b67e0bdabc7fb71b8d371f8602fe
lychz <lyc_hz@outlook.com>
2020-06-23 22:39:32 +0800

    finish Checkbox component

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -1,16 +1,86 @@
-import React, { ReactNode } from "react";
+import React, { ReactNode, useState, MouseEventHandler, useEffect, useRef } from "react";
 import "./Checkbox.scss";
-import { scopedClass } from "@utils/index";
+import {
+  combineClasses,
+  scopedClass,
+  classesObj,
+  updateStateOnPropChange,
+} from "@utils/index";
+import Icon from "@components/Icon";
+
+export interface checkAttrs {
+  children: ReactNode;
+  value: any;
+  disabled: boolean;
+  checked: boolean;
+  indeterminate: boolean;
+}
 
 interface Props {
-
+  children?: ReactNode;
+  /* Checkbox 的值，可用于与其他 Checkbox 区分 **/
+  value: any;
+  /* 选中状态 **/
+  checked?: boolean;
+  /* 禁用状态 **/
+  disabled?: boolean;
+  /* 设置 indeterminate 状态，只负责样式控制 **/
+  indeterminate?: boolean;
+  /* 点击时调用 **/
+  onClick?: (checkAttrs: checkAttrs) => {};
 }
 
 const Checkbox: React.FunctionComponent<Props> = ({
-
+  children,
+  value,
+  checked = false,
+  disabled = false,
+  indeterminate = false,
+  onClick,
 }: Props) => {
+  const checkboxClass = (...classes: (string | Array<string> | classesObj)[]) =>
+    scopedClass("checkbox", ...classes);
+
+  const [checkedStatus, setCheckedStatus] = useState(checked);
+  const check = () => {
+    setCheckedStatus(!checkedStatus);
+  };
+
+  const checkboxClassName = combineClasses({
+    [checkboxClass()]: true,
+    [checkboxClass("checked")]: checkedStatus,
+    [checkboxClass("disabled")]: disabled,
+    [checkboxClass("indeterminate")]: !checkedStatus && indeterminate,
+  });
+
+  const clickHandler: MouseEventHandler<HTMLElement> = (e) => {
+    if (disabled) {
+      return;
+    }
+    check();
+    onClick &&
+      onClick({
+        children,
+        value,
+        disabled,
+        checked: !checkedStatus,
+        indeterminate,
+      });
+  };
+
+  updateStateOnPropChange(checked, setCheckedStatus);
+
+
+
   return (
-    <div></div>
+    <span className={checkboxClassName} onClick={clickHandler}>
+      <span className={checkboxClass("inner")}>
+        {checkedStatus ? (
+          <Icon name="check" fill="#fff" size="10px"></Icon>
+        ) : null}
+      </span>
+      <span className={checkboxClass("name")}>{children}</span>
+    </span>
   );
 };
 
---
+++ src/components/Checkbox/CheckboxGroup.tsx
@@ -0,0 +1,63 @@
+import React, { useState, ReactElement, useRef, useEffect } from "react";
+import { checkAttrs } from "./Checkbox";
+import "./Checkbox.scss";
+import { scopedClass, classesObj, updateStateOnPropChange } from "@utils/index";
+
+interface Props {
+  children: ReactElement;
+  value?: Array<any>;
+  onChange?: (checkedArr?: Array<any>) => {};
+}
+
+const CheckGroup: React.FunctionComponent<Props> = ({
+  children,
+  value,
+  onChange,
+}: Props) => {
+  const radioGroupClass = (
+    ...classes: (string | Array<string> | classesObj)[]
+  ) => scopedClass("radio-group", ...classes);
+
+  const [checkedValues, setCheckedValues] = useState(value);
+
+  const changeChecked = (checkAttrs: checkAttrs) => {
+    React.Children.forEach(children, (element) => {
+      const { value } = element.props;
+      if (value === checkAttrs.value) {
+        const values = checkedValues
+          ? checkAttrs.checked
+            ? checkedValues.concat([checkAttrs.value])
+            : checkedValues.filter((v: any) => v !== checkAttrs.value)
+          : undefined;
+        setCheckedValues(values);
+        onChange && onChange(values);
+      }
+    });
+  };
+
+  updateStateOnPropChange(value, setCheckedValues);
+
+  return (
+    <div className={radioGroupClass()}>
+      {React.Children.map(children, (element) => {
+        return React.cloneElement(
+          element,
+          Object.assign(
+            {},
+            checkedValues
+              ? {
+                  checked: checkedValues.includes(element.props.value),
+                  onClick: changeChecked,
+                }
+              : {
+                  onClick: changeChecked,
+                },
+            element.props
+          )
+        );
+      })}
+    </div>
+  );
+};
+
+export default CheckGroup;
